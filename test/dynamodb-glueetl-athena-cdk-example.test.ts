7c51b8ff7d7dd27f4eb71911abbb7d9a24055a1b
Ankit Patterson <dev@weirdion.com>
2025-11-06 07:44:35 -0500

    Initial commit

---
+++ test/dynamodb-glueetl-athena-cdk-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamodbGlueetlAthenaCdkExample from '../lib/dynamodb-glueetl-athena-cdk-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamodb-glueetl-athena-cdk-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamodbGlueetlAthenaCdkExample.DynamodbGlueetlAthenaCdkExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
