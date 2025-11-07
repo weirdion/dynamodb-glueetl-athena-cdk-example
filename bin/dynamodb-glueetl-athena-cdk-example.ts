7b563a426c0d5873438918489c1c701ec03ede7c
Ankit Patterson <dev@weirdion.com>
2025-11-06 19:58:30 -0500

    Add data stack

--- bin/dynamodb-glueetl-athena-cdk-example.ts
+++ bin/dynamodb-glueetl-athena-cdk-example.ts
@@ -1,20 +1,77 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { DynamodbGlueetlAthenaCdkExampleStack } from '../lib/dynamodb-glueetl-athena-cdk-example-stack';
+import { App, Tags } from 'aws-cdk-lib';
+import { DataStack } from '../lib/stacks/data-stack';
+import { CrawlerDynamoDbStack } from '../lib/stacks/crawler-dynamodb-stack';
+import { ExportJsonStack } from '../lib/stacks/export-json-stack';
+import { ExportIonStack } from '../lib/stacks/export-ion-stack';
+import { EtlParquetStack } from '../lib/stacks/etl-parquet-stack';
 
-const app = new cdk.App();
-new DynamodbGlueetlAthenaCdkExampleStack(app, 'DynamodbGlueetlAthenaCdkExampleStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+/**
+ * DynamoDB to Athena Data Pipeline Example
+ *
+ * This CDK app demonstrates four different approaches to extract data from DynamoDB
+ * and query it via Athena, highlighting the pitfalls and the recommended solution.
+ *
+ * Stack deployment order:
+ * 1. DataStack - Foundation (DynamoDB, S3, Glue DB, Athena)
+ * 2. CrawlerDynamoDbStack - Approach 1 (Crawler reads DynamoDB directly - fails in Athena)
+ * 3. ExportJsonStack - Approach 2 (DynamoDB Export JSON - problematic)
+ * 4. ExportIonStack - Approach 3 (DynamoDB Export ION - problematic)
+ * 5. EtlParquetStack - Approach 4 (Glue ETL Parquet - recommended)
+ */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new App();
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+Tags.of(app).add('project', 'dynamodb-glueetl-athena-cdk-example');
+
+// Deploy foundation infrastructure
+const dataStack = new DataStack(app, 'DataStack', {
+  env,
+  description: 'Foundation stack with DynamoDB, S3, Glue Database, and Athena Workgroup',
 });
+
+// Approach 1 - Deploy Crawler reading DynamoDB directly (demonstrates Athena incompatibility)
+const crawlerDynamoDbStack = new CrawlerDynamoDbStack(app, 'CrawlerDynamoDbStack', {
+  env,
+  databaseName: dataStack.databaseName,
+  tableName: dataStack.table.tableName,
+  tableArn: dataStack.table.tableArn,
+  description: 'Approach 1: Glue Crawler reads DynamoDB directly (Athena cannot query non-S3 sources)',
+});
+crawlerDynamoDbStack.addDependency(dataStack);
+
+// Approach 2 - Deploy JSON Export approach (demonstrates nested JSON problem)
+const exportJsonStack = new ExportJsonStack(app, 'ExportJsonStack', {
+  env,
+  bucketName: dataStack.bucket.bucketName,
+  databaseName: dataStack.databaseName,
+  tableName: dataStack.table.tableName,
+  description: 'Approach 2: DynamoDB Export to JSON (demonstrates nested structure problem)',
+});
+exportJsonStack.addDependency(dataStack);
+
+// Approach 3 - Deploy ION Export approach (demonstrates Athena incompatibility)
+const exportIonStack = new ExportIonStack(app, 'ExportIonStack', {
+  env,
+  bucketName: dataStack.bucket.bucketName,
+  databaseName: dataStack.databaseName,
+  tableName: dataStack.table.tableName,
+  description: 'Approach 3: DynamoDB Export to ION (demonstrates Athena SerDe incompatibility)',
+});
+exportIonStack.addDependency(dataStack);
+
+// Approach 4 - Deploy Glue ETL with Parquet approach (recommended solution)
+const etlParquetStack = new EtlParquetStack(app, 'EtlParquetStack', {
+  env,
+  bucketName: dataStack.bucket.bucketName,
+  databaseName: dataStack.databaseName,
+  tableName: dataStack.table.tableName,
+  tableArn: dataStack.table.tableArn,
+  description: 'Approach 4: Glue ETL to Parquet (recommended solution with clean schema)',
+});
+etlParquetStack.addDependency(dataStack);
---
+++ lib/stacks/data-stack.ts
@@ -0,0 +1,184 @@
+import {
+  Aws,
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  Tags
+} from 'aws-cdk-lib';
+import { CfnWorkGroup } from 'aws-cdk-lib/aws-athena';
+import {
+  AttributeType,
+  BillingMode,
+  Table,
+  TableEncryption
+} from 'aws-cdk-lib/aws-dynamodb';
+import { CfnDatabase } from 'aws-cdk-lib/aws-glue';
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime
+} from 'aws-cdk-lib/aws-lambda';
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption
+} from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+/**
+ * DataStack - Foundation stack providing shared infrastructure
+ *
+ * This stack creates:
+ * - DynamoDB table with sample data structure
+ * - S3 bucket with organized prefixes for different data formats
+ * - Glue Database for catalog tables
+ * - Athena Workgroup for querying
+ * - Lambda function for seeding data (manual invocation)
+ */
+export class DataStack extends Stack {
+  public readonly table: Table;
+  public readonly bucket: Bucket;
+  public readonly database: CfnDatabase;
+  public readonly databaseName: string;
+  public readonly workgroup: CfnWorkGroup;
+  public readonly seederFunction: LambdaFunction;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table
+    this.table = new Table(this, 'SampleDataTable', {
+      tableName: 'sample-data-table',
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecoverySpecification: {
+        pointInTimeRecoveryEnabled: true, // Required for DynamoDB Export
+      },
+      removalPolicy: RemovalPolicy.DESTROY, // For demo purposes
+      encryption: TableEncryption.AWS_MANAGED,
+    });
+
+    // S3 Bucket for data storage
+    this.bucket = new Bucket(this, 'DataPipelineBucket', {
+      bucketName: `data-pipeline-bucket-${Aws.ACCOUNT_ID}-${Aws.REGION}`,
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY, // For demo purposes
+      autoDeleteObjects: true, // For demo purposes
+      lifecycleRules: [
+        {
+          id: 'DeleteOldExports',
+          prefix: 'exports/',
+          expiration: Duration.days(30),
+        },
+        {
+          id: 'DeleteOldAthenaResults',
+          prefix: 'athena-results/',
+          expiration: Duration.days(7),
+        },
+      ],
+    });
+
+    // Glue Database
+    this.databaseName = 'data_pipeline_db';
+    this.database = new CfnDatabase(this, 'DataPipelineDatabase', {
+      catalogId: Aws.ACCOUNT_ID,
+      databaseInput: {
+        name: this.databaseName,
+        description: 'Database for DynamoDB to Athena data pipeline examples',
+      },
+    });
+
+    // Athena Workgroup
+    this.workgroup = new CfnWorkGroup(this, 'DataPipelineWorkgroup', {
+      name: 'data-pipeline-workgroup',
+      description: 'Workgroup for querying DynamoDB export data',
+      workGroupConfiguration: {
+        resultConfiguration: {
+          outputLocation: `s3://${this.bucket.bucketName}/athena-results/`,
+          encryptionConfiguration: {
+            encryptionOption: 'SSE_S3',
+          },
+        },
+        enforceWorkGroupConfiguration: true,
+        publishCloudWatchMetricsEnabled: false, // Disable to reduce costs for demo
+      },
+    });
+
+    // Lambda Function for Data Seeding
+    this.seederFunction = new LambdaFunction(this, 'DataSeederFunction', {
+      functionName: 'data-seeder',
+      runtime: Runtime.PYTHON_3_11,
+      handler: 'index.handler',
+      code: Code.fromAsset(join(__dirname, '../../resources/lambda/data-seeder')),
+      timeout: Duration.minutes(5),
+      environment: {
+        TABLE_NAME: this.table.tableName,
+      },
+      description: 'Seeds DynamoDB table with structured mock data',
+    });
+
+    // Grant Lambda permissions to write to DynamoDB
+    this.table.grantWriteData(this.seederFunction);
+
+    // Add resource tags
+    Tags.of(this).add('Project', 'DynamoDB-Glue-Athena-Pipeline');
+    Tags.of(this).add('Stack', 'DataStack');
+    Tags.of(this).add('Purpose', 'Demo');
+
+    // CloudFormation Outputs
+    new CfnOutput(this, 'TableName', {
+      value: this.table.tableName,
+      description: 'DynamoDB table name',
+      exportName: 'DataPipelineTableName',
+    });
+
+    new CfnOutput(this, 'TableArn', {
+      value: this.table.tableArn,
+      description: 'DynamoDB table ARN',
+      exportName: 'DataPipelineTableArn',
+    });
+
+    new CfnOutput(this, 'BucketName', {
+      value: this.bucket.bucketName,
+      description: 'S3 bucket name',
+      exportName: 'DataPipelineBucketName',
+    });
+
+    new CfnOutput(this, 'BucketArn', {
+      value: this.bucket.bucketArn,
+      description: 'S3 bucket ARN',
+      exportName: 'DataPipelineBucketArn',
+    });
+
+    new CfnOutput(this, 'DatabaseName', {
+      value: this.databaseName,
+      description: 'Glue database name',
+      exportName: 'DataPipelineDatabaseName',
+    });
+
+    new CfnOutput(this, 'WorkgroupName', {
+      value: this.workgroup.name,
+      description: 'Athena workgroup name',
+      exportName: 'DataPipelineWorkgroupName',
+    });
+
+    new CfnOutput(this, 'SeederFunctionName', {
+      value: this.seederFunction.functionName,
+      description: 'Lambda seeder function name',
+      exportName: 'DataPipelineSeederFunctionName',
+    });
+
+    new CfnOutput(this, 'SeederInvokeCommand', {
+      value: `aws lambda invoke --function-name ${this.seederFunction.functionName} response.json`,
+      description: 'Command to invoke the data seeder',
+    });
+  }
+}
